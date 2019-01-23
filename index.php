c00778c8e4b16c209ced7202dd12bf7c0d98c165
Chad Thomas II <chad@Skylar-2.local>
2019-01-23 14:50:16 -0500

    Finished Landing Pages bio, and layout.

--- index.php
+++ index.php
@@ -21,6 +21,28 @@
 
         <div class="landing-bio">
 
+            <div class="landing-bio-container landing-bio-left">
+                <h1>Swati Dlamini Mandela</h1>
+                <hr>
+
+                <p>
+                    Princess Zamaswazi (Swati) Mandela</b>, has a story to tell. 
+                    It is not merely the chronicle of a young girl growing up in the 
+                    shadow of arguably the most influential couple of the 20th century, 
+                    her grandparents Nelson and Winnie Mandela, although that is certainly 
+                    a part of it.
+                </p>
+
+                <p>
+                Rather, Swati’s is the story of a high-achieving young South African 
+                woman who... <span><a href="/biography">Read Full Bio</a></span>
+                </p>
+            </div>
+
+            <div class="landing-bio-container landing-bio-right">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/Fjd7WLypUOc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+
         </div>
 
         
@@ -28,6 +50,7 @@
         
         
  
+    <?php include_once("footer.php");?>
 
     <?php include_once("scripts.php"); ?>
 

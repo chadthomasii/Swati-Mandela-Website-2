e437b8a2596bee172dd9dd9ca9f2d3a62cec5fb1
Chad Thomas II <chad@Skylar-2.local>
2019-01-27 16:13:21 -0500

    Added New Nav Bar.

--- js/nav.js
+++ js/nav.js
@@ -1,33 +1,18 @@
 
 // NAVIGATION
 
-var navToggle = document.querySelector(".nav-toggle");
-var navContainer = document.querySelector(".nav-menu");
 
-var open = false;
+$("nav div").click(function() {
+    $("ul").slideToggle();
+    $("ul ul").css("display", "none");
+});
 
+$("ul li").click(function() {
+    $(this).find('ul').slideToggle();
+});
 
-
-
-navToggle.addEventListener("click",toggleNav, false);
-
-
-
-//Navigation functionality
-function toggleNav()
-{
-    //If nav is not open open it
-    if(open == false)
-    {
-        navContainer.classList.add("nav-container-active");
-        open = true;
+$(window).resize(function() {
+    if($(window).width() > 768) {
+          $("ul").removeAttr('style');
     }
-
-    //If nav is open, close it
-    else if(open == true)
-    {
-        navContainer.classList.remove("nav-container-active");
-        open = false;
-    }
-}
-
+});
--- nav.php
+++ nav.php
@@ -1,67 +1,104 @@
 
 <style>
 
-nav ul
+nav
 {
-    margin: 0;
-    padding: 0;
-    list-style-type: none;
-    background-color: #fff;
-    position: relative;
-}
-
-nav li
-{
-    display: inline-block;
-    cursor:pointer;
-}
-
-nav ul li a
-{
-    color: var(--main-color);
-    text-decoration: none;
-    padding: 15px;
-    display: block;
-
-}
-nav ul li:hover [href]
-{
-    color: #fff;
-}
-
-ul li:hover,
-ul li:hover ul li
-{
-    background-color:var(--main-color);
+    box-shadow: 0 4px 2px -2px rgba(0,0,0,.2);
 
 }
 
+ul {
+      margin: 0;
+      padding: 0;
+      list-style-type: none;
+      background: #fff;
+      position: relative;
 
-/* Sub Menu */
-
-nav ul ul
-{
-    position: absolute;
-    min-width: 200px;
+}
+ 
+ul li {
+      display: inline-block;
+}
+ 
+ul li a {
+      color: var(--main-color);
+      text-decoration: none;
+      padding: 15px;
+      display: block;
+}
+ 
+ul li:hover {
+    background-color: #f3f3f3;
 }
 
-ul ul li
-{
-    display: block;
+ 
+ul ul {
+      position: absolute;
+      min-width: 200px;
+      background: lightgrey;
+      display: none;
 }
-
-
-
+ 
+ul ul li {
+      display: block;
+      background: #fff;
+}
+ 
+ul li:hover ul {
+      display: block;
+}
+ 
+ul li i {
+      color: var(--main-color);
+      float: right;
+      padding-left: 5px;
+}
+ 
+nav div {
+      background: #fff;
+      color: var(--text-color);
+      font-size: 24px;
+      padding: 0.6em;
+      cursor: pointer;
+      display: none;
+}
+ 
+@media(max-width: 768px) {
+      nav div {
+            display: block;
+      }
+ 
+      ul {
+            display: none;
+            position: static;
+            background: #fff;
+      }
+ 
+      ul li {
+            display: block;
+      }
+ 
+      ul ul {
+            position: static;
+            background: #fff;
+      }
+}
 
 </style>
 
 <nav>
-    <ul>
-        <li><a class="tag" href="/">Home</a></li>
+
+    <div>
+    
+        <i class="fa fa-bars"></i>
+    </div>
+
+    <ul class="main-menu">
+        <li><a href="/">Home</a></li>
         <li><a href="/about">About</a></li>
-        <li><a href="#">Speaking</a>
+        <li><a href="#">Speaking <i class="fas fa-caret-down"></i></a>
         
-            <ul>
+            <ul class="sub-menu">
                 <li><a href="PressKit.docx">Press Kit</a></li>
                 <li><a href="/reviews">Reviews</a></li>
 
